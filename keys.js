01c71edfd5a8480735c3f16b7bd321d79e6cf9e1
rickiangela <rickiangela@gmail.com>
2019-02-05 21:07:08 -0500

    two working functions

--- keys.js
+++ keys.js
@@ -1,13 +1,8 @@
 // console.log("They are connected!");
 
-var spotifyID = "e35350a0950649f8bc371b92f72a27be";
-var spotifySecret = "90b40b4f7da247bdbafb9f1802906843";
-var ombd = "bad7e984";
-var bandsInTown = "codingbootcamp";
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
 
-module.exports = {
-    spotifyID: spotifyID,
-    spotifySecret: spotifySecret,
-    ombd: ombd,
-    bandsInTown: bandsInTown
-};
+  
--- liri.js
+++ liri.js
@@ -8,46 +8,64 @@ var object = process.argv[3];
 // console.log(command);
 // console.log(object);
 
-var Spotify = require("node-spotify-api");
+var Spotify = require('node-spotify-api');
 var input = process.argv;
-var request = require("request");
+var request = require('request');
 var fs = require('fs');
 var axios = require("axios");
 var moment = require("moment");
 
-// console.log(keys.spotifyID);
-// console.log(keys.spotifySecret);
-// console.log(keys.ombd);
-// console.log(keys.bandsInTown);
+
 
 switch (command) {
     case "concert-this":
-    concertThis(object);
-    break;
+        concertThis(object);
+        break;
 
     case "spotify-this-song":
-    spotifyThisSong(object);
-    break;
+        spotifyThisSong(object);
+        break;
 
     case "movie-this":
-    movieThis(object);
-    break;
+        movieThis(object);
+        break;
 
     case "do-what-it-says":
-    doWhatItSays();
-	break;
+        doWhatItSays();
+        break;
 }
 
-function concertThis(band){
-    console.log("You picked the band " + band);
+function concertThis(band) {
+    axios.get("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp").then(
+        function (response) {
+
+                console.log("Venue Name: " + response.data[i].venue.name);
+                console.log("Location: " + response.data[i].venue.city);
+                console.log("Date: " + moment(response.data[i].datetime).format('MMMM Do YYYY'));
+        });
 }
 
 function spotifyThisSong(song) {
-    console.log("You picked the song " + song);
+
 }
 
 function movieThis(movie) {
-    console.log("You picked the movie " + movie);
+    axios.get("http://www.omdbapi.com/?t=" + movie + "&apikey=trilogy").then(
+        function (response) {
+            
+            console.log("Title: " + response.data.Title);
+            console.log("Released: " + response.data.Year);
+            console.log("IMDB rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
+            console.log("Produced: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+
+
+        }
+
+    );
 }
 
 function doWhatItSays() {

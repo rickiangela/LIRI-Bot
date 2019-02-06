8d03ee57dbfbeee3f1fca51c7dab2936bb5199dd
rickiangela <rickiangela@gmail.com>
2019-02-05 20:18:17 -0500

    trying to get this

---
+++ keys.js
@@ -0,0 +1,13 @@
+// console.log("They are connected!");
+
+var spotifyID = "e35350a0950649f8bc371b92f72a27be";
+var spotifySecret = "90b40b4f7da247bdbafb9f1802906843";
+var ombd = "bad7e984";
+// var bandsInTown;
+
+module.exports = {
+    spotifyID: spotifyID,
+    spotifySecret: spotifySecret,
+    ombd: ombd,
+    // bandsInTown: bandsInTown
+};
--- liri.js
+++ liri.js
@@ -1,1 +1,49 @@
- 
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var command = process.argv[2];
+var object = process.argv[3];
+
+// console.log(command);
+// console.log(object);
+
+var spotify = require("node-spotify-api");
+var input = process.argv;
+var request = require("request");
+var fs = require('fs');
+var axios = require("axios");
+
+// console.log(keys.spotifyID);
+// console.log(keys.spotifySecret);
+// console.log(keys.ombd);
+
+switch (command) {
+    // case "concert-this":
+    // concertThis(object);
+    // break;
+
+    case "spotify-this-song":
+    spotifyThisSong(object);
+    break;
+
+    case "movie-this":
+    movieThis(object);
+    break;
+
+    case "do-what-it-says":
+    doWhatItSays();
+	break;
+}
+
+function spotifyThisSong(song) {
+    console.log("You picked the song " + song);
+}
+
+function movieThis(movie) {
+    console.log("You picked the movie " + movie);
+}
+
+function doWhatItSays() {
+    console.log("This will run 'I Want It That Way'.");
+}
